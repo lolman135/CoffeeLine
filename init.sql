5e3a15660d6c34c6cd1e4f7e104a7d164090f712
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2025-11-18 13:41:52 +0200

    Add docker support

--- init.sql
+++ init.sql
@@ -1,83 +1,366 @@
+--
+-- PostgreSQL database cluster dump
+--
 
--- user info --
-CREATE TABLE IF NOT EXISTS users(
-	id UUID NOT NULL PRIMARY KEY, 
-	name VARCHAR(50) NOT NULL, 
-	email VARCHAR(100) NOT NULL,
-	phone_number VARCHAR(15) NOT NULL,
-	password TEXT NOT NULL
+-- Started on 2025-11-18 13:04:48
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+\connect template1
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5 (Debian 17.5-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-11-18 13:04:49
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2025-11-18 13:04:49
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "coffee_line_db" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5 (Debian 17.5-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-11-18 13:04:49
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3400 (class 1262 OID 16384)
+-- Name: coffee_line_db; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE coffee_line_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE coffee_line_db OWNER TO postgres;
+
+\connect coffee_line_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 219 (class 1259 OID 16397)
+-- Name: categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.categories (
+    id uuid NOT NULL,
+    name character varying(25) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS roles(
-	id UUID NOT NULL PRIMARY KEY,
-	name VARCHAR(25) NOT NULL
+
+ALTER TABLE public.categories OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 16402)
+-- Name: coffees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.coffees (
+    id uuid NOT NULL,
+    name character varying(50) NOT NULL,
+    description text NOT NULL,
+    price double precision NOT NULL,
+    image_url text NOT NULL,
+    category_id uuid NOT NULL
 );
 
--- coffe info --
-CREATE TABLE IF NOT EXISTS categories(
-	id UUID NOT NULL PRIMARY KEY,
-	name VARCHAR(25) NOT NULL
+
+ALTER TABLE public.coffees OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 24606)
+-- Name: order_items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.order_items (
+    id uuid NOT NULL,
+    quantity integer NOT NULL,
+    coffee_id uuid,
+    order_id uuid
 );
 
-CREATE TABLE IF NOT EXISTS coffees(
-	id UUID NOT NULL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	description TEXT NOT NULL,
-	price INT NOT NULL,
-	image_url TEXT NOT NULL,
-	category_id UUID NOT NULL,
-	CONSTRAINT fk_coffee_category
-		FOREIGN KEY (category_id)
-		REFERENCES categories(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+
+ALTER TABLE public.order_items OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16414)
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    total_cost double precision NOT NULL,
+    status character varying(20) NOT NULL,
+    user_id uuid NOT NULL
 );
 
--- order info --
-CREATE TABLE IF NOT EXISTS orders(
-	id UUID NOT NULL PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL,
-	total_cost INT NOT NULL,
-	status VARCHAR(20) NOT NULL,
-	user_id UUID NOT NULL,
-	CONSTRAINT fk_order_user
-		FOREIGN KEY (user_id)
-		REFERENCES users(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 16392)
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    id uuid NOT NULL,
+    name character varying(25) NOT NULL
 );
 
--- many to many relation tables
-CREATE TABLE IF NOT EXISTS users_roles(
-	user_id UUID NOT NULL,
-	role_id UUID NOT NULL,
-	CONSTRAINT fk_user_id 
-		FOREIGN KEY (user_id) 
-		REFERENCES users(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE,
-	CONSTRAINT fk_role_id 
-		FOREIGN KEY (role_id) 
-		REFERENCES roles(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+
+ALTER TABLE public.roles OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 16385)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    name character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    phone_number character varying(15) NOT NULL,
+    password text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS order_items(
-	id UUID NOT NULL PRIMARY KEY,
-	order_id UUID NOT NULL,
-	coffee_id UUID NOT NULL,
-	quantity INT NOT NULL,
-	CONSTRAINT fk_order_id
-		FOREIGN KEY (order_id)
-		REFERENCES orders(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE,
-	CONSTRAINT fk_coffee_id
-		FOREIGN KEY (coffee_id)
-		REFERENCES coffees(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-	CONSTRAINT uq_order_coffee
-		UNIQUE(order_id, coffee_id)
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 16424)
+-- Name: users_roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users_roles (
+    user_id uuid NOT NULL,
+    roles character varying(100) NOT NULL
 );
+
+
+ALTER TABLE public.users_roles OWNER TO postgres;
+
+--
+-- TOC entry 3238 (class 2606 OID 16401)
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3240 (class 2606 OID 16408)
+-- Name: coffees coffees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coffees
+    ADD CONSTRAINT coffees_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3244 (class 2606 OID 24610)
+-- Name: order_items order_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3242 (class 2606 OID 16418)
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3236 (class 2606 OID 16396)
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3234 (class 2606 OID 16391)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3245 (class 2606 OID 16409)
+-- Name: coffees fk_coffee_category; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coffees
+    ADD CONSTRAINT fk_coffee_category FOREIGN KEY (category_id) REFERENCES public.categories(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3246 (class 2606 OID 16419)
+-- Name: orders fk_order_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3247 (class 2606 OID 16427)
+-- Name: users_roles fk_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_roles
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3248 (class 2606 OID 24615)
+-- Name: order_items fkbcy00h41vbjyga9cpuw22hxu6; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT fkbcy00h41vbjyga9cpuw22hxu6 FOREIGN KEY (coffee_id) REFERENCES public.coffees(id);
+
+
+--
+-- TOC entry 3249 (class 2606 OID 24620)
+-- Name: order_items fkbioxgbv59vetrxe0ejfubep1w; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT fkbioxgbv59vetrxe0ejfubep1w FOREIGN KEY (order_id) REFERENCES public.orders(id);
+
+
+-- Completed on 2025-11-18 13:04:49
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+\connect postgres
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5 (Debian 17.5-1.pgdg120+1)
+-- Dumped by pg_dump version 17.0
+
+-- Started on 2025-11-18 13:04:49
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2025-11-18 13:04:50
+
+--
+-- PostgreSQL database dump complete
+--
+
+-- Completed on 2025-11-18 13:04:50
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
